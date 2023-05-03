157618db4fd0784734866dcc7425e7c0fa0e7051
starkjoy <residualcode6@gmail.com>
2023-05-03 18:32:52 +0000

    Regex

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/.(from:.{12})./).join
-puts ARGV[1].scan(/.(to:.{12})./).join
-puts ARGV[2].scan(/.(flags:.{12})./).join
+puts ARGV[0].scan(/(.(from:.{12}).)(.(to:.{12}).)(.(flags:.{12}).)/).join
