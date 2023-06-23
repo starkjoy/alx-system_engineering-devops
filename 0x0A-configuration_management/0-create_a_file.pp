b20c192de99d2ccad5493c11863887e8f6e77059
starkjoy <residualcode6@gmail.com>
2023-06-23 06:58:47 -0700

    Puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,11 +1,11 @@
 #Creates a file
 
 file { 'school':
-	ensure	=> 'file',
-	path	=> '/tmp/school',
-	mode	=> '0744',
-	owner	=> 'www-data',
-	group	=> 'www-data',
-	content	=> 'I love Puppet',
+  ensure => 'file',
+  path => '/tmp/school',
+  mode => '0744',
+  owner => 'www-data',
+  group => 'www-data',
+  content => 'I love Puppet',
 }
 
