1081ac3495051b82aafabe329bd155bd8a30e0b7
Joy <117626913+starkjoy@users.noreply.github.com>
2023-08-08 20:04:23 +0000

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-""" Functions to query subscribers on a given Reddit subreddit"""
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
-def numbers_of_subscribers(subreddit):
-    """Queries the Reddit API to retrieve the subreddit subscribers"""
-
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
     response = requests.get(url, headers=headers, allow_redirects=False)
-
     if response.status_code == 404:
         return 0
     results = response.json().get("data")
