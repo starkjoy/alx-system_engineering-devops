4f9503606f76e337efca6459288e19d6464646ad
starkjoy <residualcode6@gmail.com>
2023-08-08 17:23:28 +0000

    Api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import requests
+
+
+def numbers_of_subscribers(subreddit):
+    """
+    Queries the Reddit API to retrieve the number of subreddit subscribers
+
+    :param subreddit: The name of the subreddit.
+    :return: Number of subscribers for the subreddit, or 0 if the subreddit
+    """
+
+    headers = {'User-Agent': 'MyRedditBot/1.0'}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+
+        return subscribers
+    else:
+        return 0
