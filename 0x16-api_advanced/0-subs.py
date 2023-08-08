4073cf5de57f30fa02ba8c8f617d30456a78dbaf
starkjoy <residualcode6@gmail.com>
2023-08-08 17:43:14 +0000

    Api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,22 +6,13 @@ import requests
 def numbers_of_subscribers(subreddit):
     """
     Queries the Reddit API to retrieve the number of subreddit subscribers
-
-    :param subreddit: The name of the subreddit.
-    :return: Number of subscribers for the subreddit, or 0 if the subreddit
     """
 
-    headers = {'User-Agent': 'MyRedditBot/1.0'}
+    headers = "User-Agent": "MyRedditBot/1.0"
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-
-        return subscribers
-    else:
-        if response.status_code == 404:
+    if response.status_code == 404:
             return 0
     results = response.json().get("data")
     return results.get("subscribers")
