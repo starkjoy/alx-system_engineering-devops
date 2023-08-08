afad383cb4da2f535603212ea4bcff83db246e81
starkjoy <residualcode6@gmail.com>
2023-08-08 17:49:08 +0000

    Api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,13 +4,13 @@ import requests
 
 
 def numbers_of_subscribers(subreddit):
-    """Queries the Reddit API to retrieve the number of subreddit subscribers"""
+    """Queries the Reddit API to retrieve the subreddit subscribers"""
 
     headers = {"User-Agent": "MyRedditBot/1.0"}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 404:
-            return 0
+        return 0
     results = response.json().get("data")
     return results.get("subscribers")
