1d04162aedc2608adacf9c0de76835359ec05857
starkjoy <residualcode6@gmail.com>
2023-08-18 13:46:57 +0000

    Fixed Syntax Error

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -3,12 +3,12 @@
 # Increases the hard limit for Holberton user
 exec { 'increase-hard-limit'
 	command	=> 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
-	path	=> '/usr/local/bin/:/bin/'
+	path	=> ['/usr/local/bin', '/bin/']
 }
 
 
 # Increases the soft limit for Holberton user
 exec { 'increase-soft-limit'
 	command	=> 'sed -i "/holberton soft/s/4/5000/" /etc/security/limits.conf',
-	path	=> '/usr/local/bin/:bin/'
+	path	=> ['/usr/local/bin', '/bin/']
 }
